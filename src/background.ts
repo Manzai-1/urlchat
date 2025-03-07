ba1c0d1930e159ac42c637c41d8da2cb420ae35a
Manzai-1 <0xdecentmaz@gmail.com>
2025-03-07 18:25:16 +0100

    initial commit

--- src/background.ts
+++ src/background.ts
@@ -1,2 +1,90 @@
-export {}
-console.log("HELLO WORLD FROM BGSCRIPTS")
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  switch(msg.perform){
+    case "addmessage": {
+      const date = new Date().toISOString().slice(0, 16).replace("T", " ");
+
+      getCurrentTab().then(url => {
+        sendMessage(cleanUrl(url), msg.author, date, msg.message).then(result => {
+          sendResponse({data: result})
+        });
+      });
+
+      return true
+    }
+    case "getmessages": {
+
+      getCurrentTab().then(url => {
+        fetchMessages(cleanUrl(url)).then(response => {
+          sendResponse({data: sortMessages(response)});
+        })
+      })
+      return true
+    }
+  }
+})
+
+const sortMessages = (messages:[any]) => {
+  return messages.sort((b, a) => {
+    const aDate:any = new Date(a.date);
+    const bDate:any = new Date(b.date);
+    return aDate - bDate;
+  });
+}
+
+async function getCurrentTab() {
+  const queryOptions = { active: true, lastFocusedWindow: true };
+  const [tab] = await chrome.tabs.query(queryOptions);
+  return tab.url;
+}
+
+async function fetchMessages(url) {
+  const endPoint = "https://us-central1-urlchat-93ae4.cloudfunctions.net/getmessages"
+  try {
+    const response = await fetch(endPoint, {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({"url":url})
+    })
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error(`${response.status} - ${endPoint}`)
+    }
+  } catch (error) {
+    return `Error: ${error}`
+  }
+}
+
+async function sendMessage(url, author, date, message) {
+  const endPoint = "https://us-central1-urlchat-93ae4.cloudfunctions.net/addmessage"
+  try {
+    const response = await fetch(endPoint, {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        "url":url,
+        "author":author,
+        "date":date,
+        "message":message
+      })
+    })
+    if (response.ok) {
+      return await response.json()
+    } else {
+      throw new Error(`${response.status} - ${endPoint}`)
+    }
+  } catch (error) {
+    return `Error: ${error}`
+  }
+}
+
+
+const cleanUrl = (str) => {
+  return [...str].filter(isAccepterChar).join('');
+}
+
+const isAccepterChar = (c) => { 
+  let cc = c.charCodeAt(0);
+  return (cc >= 65 && cc <= 90) || (cc >= 97 && cc <= 122)
+}
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,20 +1,75 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
+import "./style.css"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [message, setMessage] = useState("")
+  const [messages, setMessages] = useState([
+    { id: "", author: "", date: "", message: "" }
+  ])
+  const [user, setUser] = useState("maz")
+  const [hasFetched, setHasFetched] = useState(false)
+
+  useEffect(() => {
+    if (hasFetched) {
+      return
+    }
+    fetchMessages()
+    setHasFetched(true)
+  })
+
+  const fetchMessages = () => {
+    chrome.runtime.sendMessage({ perform: "getmessages" }, (response) => {
+      setMessages(response.data)
+    })
+  }
+
+  const handleInputChange = (e) => {
+    setMessage(e.target.value)
+  }
+
+  const handleInputSave = (e) => {
+    e.preventDefault()
+    chrome.runtime.sendMessage(
+      { perform: "addmessage", author: user, message: message },
+      (response) => {
+        console.log(response.data)
+        fetchMessages()
+      }
+    )
+    e.target.firstChild.value = ""
+  }
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-      </h1>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <footer>Crafted by @PlasmoHQ</footer>
+    <div className="popup-container">
+      <form onSubmit={handleInputSave} className="message-form">
+        <input
+          type="text"
+          onChange={handleInputChange}
+          placeholder="Type your message..."
+          className="message-input"
+        />
+        <button type="submit" className="send-button">
+          Send
+        </button>
+      </form>
+      <button onClick={fetchMessages} className="refresh-button">
+        Refresh Messages
+      </button>
+      <ul className="message-list">
+        {messages[0].id &&
+          messages.map((m) => {
+            return (
+              <li key={m.id} className="message-item">
+                <div className="message-meta">
+                  <p className="message-author">{m.author}</p>
+                  <p className="message-date">{m.date}</p>
+                </div>
+                <p className="message-text">{m.message}</p>
+              </li>
+            )
+          })}
+      </ul>
     </div>
   )
 }
